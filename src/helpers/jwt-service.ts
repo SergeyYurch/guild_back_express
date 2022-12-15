d14af350aec505aa7deea9cc62d4c0488efa795d
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-15 14:50:29 +0200

    refactor jwt-service.ts

--- src/helpers/jwt-service.ts
+++ src/helpers/jwt-service.ts
@@ -3,16 +3,36 @@ import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET || '1';
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || '11';
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || '22';
+
+const expiresIn_Access = '10s';
+
+export type TypeJWT = 'access' | 'refresh'
+
 
 export const jwtService = {
-    async createJWT(id:string) {
-        return jwt.sign({userId: id}, JWT_SECRET, {expiresIn: '10h'});
+    async createAccessJWT(id: string) {
+        return jwt.sign(
+            {userId: id},
+            JWT_ACCESS_SECRET,
+            {expiresIn: expiresIn_Access}
+        );
     },
 
-    async getUserIdByJwtToken(token: string) {
+    async createRefreshJWT(id: string) {
+        return jwt.sign(
+            {userId: id},
+            JWT_REFRESH_SECRET,
+        );
+    },
+
+    async getUserIdByJwtToken(token: string, type: TypeJWT) {
         try {
-            const result: any = jwt.verify(token, JWT_SECRET);
+            const result: any = jwt.verify(
+                token,
+                type === 'access' ? JWT_ACCESS_SECRET : JWT_REFRESH_SECRET
+            );
             return result.userId;
         } catch (error) {
             return null;
