0fedd4dbec6cc762298a8af98e5d089e8406a82e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-15 14:48:08 +0200

    refactor auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -22,25 +22,34 @@ const {
     findUserByEmailOrPassword,
     registerNewUser,
     confirmEmail,
-    resendingEmail
+    resendingEmail,
+    userLogin,
+    userLogout,
+    refreshUserTokens,
+    checkUserRefreshToken
 } = usersService;
+
+const CookieRefreshTokenExpire =()=> new Date(Date.now() + 1120000);
 
 authRouter.post('/login',
     validateLoginInputModel(),
     validateResult,
     async (req: RequestWithBody<LoginInputModel>, res: Response) => {
         const {loginOrEmail, password} = req.body;
-        console.log(`!!!![authRouter] login:${loginOrEmail}, pass: ${password}`);
+        console.log(`!!!![authRouter] login:${loginOrEmail}`);
         const user = await checkCredentials({loginOrEmail, password});
-        if (user) {
-            const token = await jwtService.createJWT(user.id);
-            return res.status(200).send({
-                "accessToken": token
-            });
-        } else {
-            return res.sendStatus(401);
-        }
-    });
+        if (!user) return res.sendStatus(401);
+        const loginParams = await userLogin(user.id);
+        res.cookie(
+            'refreshToken',
+            loginParams.refreshToken,
+            {expires: CookieRefreshTokenExpire(), secure: true, httpOnly: true}
+        );
+        return res.status(200).send({
+            "accessToken": loginParams.accessToken
+        });
+    }
+);
 
 authRouter.get('/me',
     authBearerMiddleware,
@@ -55,6 +64,7 @@ authRouter.get('/me',
             userId
         });
     });
+
 authRouter.post('/registration',
     validateUserInputModel(),
     validateResult,
@@ -77,9 +87,13 @@ authRouter.post('/registration-confirmation',
         console.log(`[authController]:POST/registration-confirmation run`);
         console.log(req.body);
         const code = String(req.body.code);
-        const result = await confirmEmail(code);
-        if (!result) res.sendStatus(400);
-        return res.sendStatus(204);
+        try {
+            const result = await confirmEmail(code);
+            if (!result) res.sendStatus(400);
+            return res.sendStatus(204);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
     });
 
 
@@ -89,10 +103,65 @@ authRouter.post('/registration-email-resending',
     async (req: RequestWithBody<RegistrationEmailResendingModelDto>, res: Response) => {
         console.log(`[authController]:POST/registration-email-resending run`);
         const {email} = req.body;
-        const user = await findUserByEmailOrPassword(email);
-        const result = await resendingEmail(user!.id);
-        if (!result) return res.status(400).send(
-            {"errorsMessages":[{"message":"cant\'t send email","field":"email"}]}
-        );
-        return res.sendStatus(204);
+        try {
+            const user = await findUserByEmailOrPassword(email);
+            const result = await resendingEmail(user!.id);
+            if (!result) return res.status(400).send(
+                {"errorsMessages": [{"message": "cant\'t send email", "field": "email"}]}
+            );
+            return res.sendStatus(204);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+    });
+
+authRouter.post('/refresh-token',
+    async (req: Request, res: Response) => {
+        console.log(`[authController]:POST/refresh-token run`);
+        const oldRefreshToken = req.cookies;
+        // console.log('oldRefreshToken');
+        // console.log(oldRefreshToken);
+        try {
+            // console.log('try in');
+            if (!oldRefreshToken) return res.sendStatus(401);
+            const userId = await jwtService.getUserIdByJwtToken(oldRefreshToken, "refresh");
+            // console.log('userId');
+            // console.log(userId);
+            if (!userId) return res.sendStatus(401);
+            const tokenIsMissing = await checkUserRefreshToken(oldRefreshToken, userId);
+            // console.log('tokenIsMissing');
+            // console.log(tokenIsMissing);
+            if (tokenIsMissing) return res.sendStatus(401);
+            const tokensPair = await refreshUserTokens(oldRefreshToken, userId);
+            // console.log('tokensPair');
+            // console.log(tokensPair);
+            if (!tokensPair) return res.sendStatus(500);
+            res.cookie(
+                'refreshToken',
+                tokensPair.refreshToken,
+                {expires: CookieRefreshTokenExpire(), secure: true, httpOnly: true}
+            );
+            return res.status(200).send({
+                "accessToken": tokensPair.accessToken
+            });
+        } catch (error) {
+            return res.sendStatus(500);
+        }
+    });
+
+authRouter.post('/logout',
+    async (req: Request, res: Response) => {
+        console.log(`[authController]:POST/logout run`);
+        const inputRefreshToken = req.cookies?.refreshToken;
+        try {
+            if (!inputRefreshToken) return res.sendStatus(401);
+            const userId = await jwtService.getUserIdByJwtToken(inputRefreshToken, 'refresh');
+            if (!userId) return res.sendStatus(401);
+            const result = await userLogout(inputRefreshToken, userId);
+            if (!result) return res.sendStatus(500);
+            res.clearCookie('refreshToken')
+            return res.sendStatus(204);
+        } catch (error) {
+            return res.sendStatus(500);
+        }
     });
