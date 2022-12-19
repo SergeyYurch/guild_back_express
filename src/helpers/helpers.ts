ac338fa4ed26206ce9855e95f7da9c2a251aa724
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-19 21:08:24 +0200

    add functional to helpers.ts

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,6 +1,13 @@
 import {Request} from "express";
 import {PaginatorOptionInterface} from "../repositories/interfaces/query.repository.interface";
 import bcrypt from "bcrypt";
+import {UserInDbEntity} from "../repositories/entitiesRepository/userInDb.entity";
+import {UserViewModelDto} from "../controllers/dto/userViewModel.dto";
+import {v4 as uuidv4} from "uuid";
+import add from "date-fns/add";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const parseQueryPaginator = (req: Request): PaginatorOptionInterface => {
     return {
@@ -13,6 +20,26 @@ export const parseQueryPaginator = (req: Request): PaginatorOptionInterface => {
 
 export const pagesCount = (totalCount: number, pageSize: number) => Math.ceil(totalCount / pageSize);
 
-export const generateHash = async (password: string, salt: string): Promise<string> => {
-    return await bcrypt.hash(password, salt);
+export const generateHash = async (password: string): Promise<{salt:string, pass:string}> => {
+    const salt_base = process.env.HASH_SALT_BASE || '54321'
+    const salt = await bcrypt.genSalt(+salt_base);
+    const pass = await bcrypt.hash(password, salt);
+    return {salt, pass}
 };
+
+export const parseUserViewModel = (user: UserInDbEntity): UserViewModelDto => {
+    return {
+        id: user.id,
+        login: user.accountData.login,
+        email: user.accountData.email,
+        createdAt: user.accountData.createdAt.toISOString()
+    };
+};
+
+export const getConfirmationCode = () => uuidv4();
+
+export const getExpirationDate = () => add(new Date(),
+    {
+        hours: 0,
+        minutes: 10
+    });
