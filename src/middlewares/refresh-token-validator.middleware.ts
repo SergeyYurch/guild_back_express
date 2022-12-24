00e550f8739d5c9e7d39b0711a0570b30d754113
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-24 20:57:04 +0200

    added refresh-token-validator.middleware.ts

---
+++ src/middlewares/refresh-token-validator.middleware.ts
@@ -0,0 +1,23 @@
+import {Request, Response, NextFunction} from 'express';
+import {jwtService} from "../utils/jwt-service";
+import {usersService} from "../services/users.service";
+import {getDeviceInfo} from "../helpers/helpers";
+import {authService} from "../services/auth.service";
+
+export const refreshTokenValidator = async (req: Request, res: Response, next: NextFunction) => {
+    console.log('[refreshTokenValidator]');
+    console.log(req.cookies );
+    const refreshToken = req.cookies?.refreshToken;
+    try {
+        console.log(`[refreshTokenValidator]: refreshToken : ${refreshToken}`);
+        if (!refreshToken) return res.sendStatus(401);
+        const {ip, title} = getDeviceInfo(req);
+        const userId = await authService.checkDeviceSession(ip, title, refreshToken);
+        console.log(`[refreshTokenValidator]: userId : ${userId}`);
+        if (!userId) return res.sendStatus(401);
+        req.user= await usersService.getUserById(userId)
+        return next();
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+};
