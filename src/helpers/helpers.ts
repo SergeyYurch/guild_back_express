c60c08a26e106bfb749cabf96e93a28cba45a1f2
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-24 20:55:11 +0200

    refactor helpers.ts

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,11 +1,12 @@
-import {Request} from "express";
+import {Request, Response} from "express";
 import {PaginatorOptionInterface} from "../repositories/interfaces/query.repository.interface";
 import bcrypt from "bcrypt";
-import {UserInDbEntity} from "../repositories/entitiesRepository/userInDb.entity";
+import {UserInDbEntity} from "../repositories/entitiesRepository/user-in-db.entity";
 import {UserViewModelDto} from "../controllers/dto/userViewModel.dto";
 import {v4 as uuidv4} from "uuid";
 import add from "date-fns/add";
 import * as dotenv from "dotenv";
+import hash from "hash.js";
 
 dotenv.config();
 
@@ -20,12 +21,12 @@ export const parseQueryPaginator = (req: Request): PaginatorOptionInterface => {
 
 export const pagesCount = (totalCount: number, pageSize: number) => Math.ceil(totalCount / pageSize);
 
-export const generateHash = async (password: string, salt:string): Promise<string> => {
-    return await bcrypt.hash(password, salt);
+export const generatePassHash = (password: string, salt: string): string => {
+    return hash.sha256().update(salt).digest('hex');
 };
 
 export const generateHashSalt = async (): Promise<string> => {
-    const salt_base = process.env.HASH_SALT_BASE || '54321'
+    const salt_base = process.env.HASH_SALT_BASE || '54321';
     return await bcrypt.genSalt(+salt_base);
 };
 
@@ -45,3 +46,20 @@ export const getExpirationDate = () => add(new Date(),
         hours: 0,
         minutes: 10
     });
+export const getCookieRefreshTokenExpire = () => add(new Date(), {seconds: 18});
+
+export const setRefreshTokenToCookie = (res: Response, refreshToken: string) => {
+    res.cookie(
+        'refreshToken',
+        refreshToken,
+        {expires: getCookieRefreshTokenExpire(),  httpOnly: true} //secure: true,
+    );
+};
+
+
+export const getDeviceInfo = (req: Request): { ip: string, title: string } => {
+    const ipInHeaders = req.headers['x-forwarded-for'];
+    const ip = Array.isArray(ipInHeaders) ? ipInHeaders[0] : (ipInHeaders || '00:00:0000');
+    const title = req.headers['user-agent'] || 'no name';
+    return {ip, title};
+};
