b64f3bdd7678ecebb17a6247669c315297fff16e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-25 22:28:11 +0200

    added settings-const.ts

---
+++ src/utils/settings-const.ts
@@ -0,0 +1,19 @@
+
+export const ACCESS_TOKEN_LIFE_PERIOD = {
+    units: 'seconds',
+    amount: '110'
+};
+export const REFRESH_TOKEN_LIFE_PERIOD = {
+    units: 'seconds',
+    amount: '120'
+};
+
+export const COOKIE_LIFE_PERIOD = {
+    units: 'seconds',
+    amount: '120'
+};
+
+export const CONFIRM_EMAIL_LIFE_PERIOD = {
+    units: 'minutes',
+    amount: '10'
+};
