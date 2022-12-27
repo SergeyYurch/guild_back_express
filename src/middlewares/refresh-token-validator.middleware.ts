a50e735ccd25feb91b891d200a6cc6809904d556
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-27 22:31:31 +0200

    refactor refresh-token-validator.middleware.ts

--- src/middlewares/refresh-token-validator.middleware.ts
+++ src/middlewares/refresh-token-validator.middleware.ts
@@ -6,19 +6,18 @@ import {authService} from "../services/auth.service";
 
 export const refreshTokenValidator = async (req: Request, res: Response, next: NextFunction) => {
     console.log('[refreshTokenValidator]');
-    console.log(req.cookies );
     const refreshToken = req.cookies?.refreshToken;
     console.log(`[refreshTokenValidator] refreshToken: ${refreshToken}`);
 
     try {
         console.log(`[refreshTokenValidator]: refreshToken : ${refreshToken}`);
-        if (!refreshToken) return res.sendStatus(401);
+        if (!refreshToken) return res.status(401).send('no refreshToken');
         const {ip, title} = getDeviceInfo(req);
         console.log(`[refreshTokenValidator]: ip : ${ip}`);
         console.log(`[refreshTokenValidator]: title : ${title}`);
         const userId = await authService.checkDeviceSession(ip, title, refreshToken);
         console.log(`[refreshTokenValidator]: userId : ${userId}`);
-        if (!userId) return res.sendStatus(401);
+        if (!userId) return res.status(402).send(`no checkDeviceSession title:${title}`);
         req.user= await usersService.getUserById(userId)
         return next();
     } catch (error) {
