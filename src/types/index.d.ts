5c585ea0fe6faed9efc1fbc026d3e27b63d45b77
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-28 23:52:37 +0200

    refactor  project

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,7 +3,8 @@ import {UserViewModelDto} from "../controllers/dto/userViewModel.dto";
 declare global{
     declare namespace Express{
         export interface Request {
-            user: UserViewModelDto | null
+            user: UserViewModelDto | null,
+            deviceId: string| undefined
         }
     }
 }
