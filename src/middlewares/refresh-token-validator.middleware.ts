9aa146df18f812828d8b10f2b048160cda79984c
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-28 23:52:50 +0200

    refactor  project

--- src/middlewares/refresh-token-validator.middleware.ts
+++ src/middlewares/refresh-token-validator.middleware.ts
@@ -5,20 +5,23 @@ import {getDeviceInfo} from "../helpers/helpers";
 import {authService} from "../services/auth.service";
 
 export const refreshTokenValidator = async (req: Request, res: Response, next: NextFunction) => {
+    // const refreshToken = req.cookies.refreshToken;
+    // if (!refreshToken) return res.sendStatus(401)
+    // const tokenPayload = jwtService.getUserIdByJwtToken()
     console.log('[refreshTokenValidator]');
     const refreshToken = req.cookies?.refreshToken;
     console.log(`[refreshTokenValidator] refreshToken: ${refreshToken}`);
 
     try {
         console.log(`[refreshTokenValidator]: refreshToken : ${refreshToken}`);
-        if (!refreshToken) return res.status(401).send('no refreshToken');
-        const {ip, title} = getDeviceInfo(req);
-        console.log(`[refreshTokenValidator]: ip : ${ip}`);
-        console.log(`[refreshTokenValidator]: title : ${title}`);
-        const result = await authService.checkDeviceSession(ip, title, refreshToken);
+        if (!refreshToken) return res.sendStatus(401)
+        if(!jwtService.verifyJwtToken(refreshToken, 'refresh')) return res.sendStatus(401)
+        const {lastActiveDate, userId, deviceId} = jwtService.getSessionInfoByJwtToken(refreshToken);
+        const result = await authService.checkDeviceSession(deviceId, userId, lastActiveDate);
         console.log(`[refreshTokenValidator]: userId : ${result}`);
-        if (result.status!=='ok') return res.status(402).send(`no checkDeviceSession error:${result.message}`);
-        req.user= await usersService.getUserById(result.message)
+        if (result.status!=='ok') return res.status(401).send(`no checkDeviceSession error:${result.message}`);
+        req.user= await usersService.getUserById(userId)
+        req.deviceId = deviceId;
         return next();
     } catch (error) {
         return res.sendStatus(500);
