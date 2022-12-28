9f0cd9ddef51b42636f56879ae9aaa5bad5b0fa1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-12-28 23:52:24 +0200

    refactor  project

--- src/adapters/dbAdapters.ts
+++ src/adapters/dbAdapters.ts
@@ -8,6 +8,7 @@ import {RefreshTokenEntity} from "../services/entities/refresh-token.entity";
 import {AuthSessionInDb} from "../repositories/entitiesRepository/auth-session-in-db.interface";
 import {AuthSessionEntity} from "../services/entities/auth-session.entity";
 import {AccessAttemptInDb} from "../repositories/entitiesRepository/access-attempt-in-db.interface";
+import {DbAuthSessionInterface} from '../repositories/entitiesRepository/db-auth-session.interface';
 dotenv.config();
 
 const mongoUri = process.env.MONGO_URI
@@ -21,7 +22,7 @@ export const postsCollection = dbAdapters.collection<PostEntity>('posts')
 export const usersCollection = dbAdapters.collection<UserEntity>('users')
 export const commentsCollection = dbAdapters.collection<CommentEntity>('comments')
 export const tokensBlackListCollection = dbAdapters.collection<RefreshTokenEntity>('tokens_black_list')
-export const deviceAuthSessionsCollection = dbAdapters.collection<AuthSessionEntity>('device_auth_sessions')
+export const deviceAuthSessionsCollection = dbAdapters.collection<DbAuthSessionInterface>('device_auth_sessions')
 export const accessAttemptCollection = dbAdapters.collection<AccessAttemptInDb>('access_attempt')
 
 export async function runDB() {
